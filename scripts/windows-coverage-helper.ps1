05cddeb504bec4ee879c3f2b767c472296a3a667
Cooper Walbrun <69944858+cooperwalbrun@users.noreply.github.com>
2021-12-30 13:27:09 -0600

    Install rustfilt in the Makefile.toml and update some command syntaxes

--- scripts/windows-coverage-helper.ps1
+++ scripts/windows-coverage-helper.ps1
@@ -5,8 +5,6 @@ $html_output_directory = "./coverage/"
 $profile_data_file = "./coverage.profdata"
 $coverage_ignore_regex = '\.cargo.registry|\.rustup' # Matches .cargo/registry and .rustup/
 
-cargo install rustfilt # There was no elegant way to include this as an install_crate in Makefile.toml
-
 # Generate log messages via Cargo that we can parse to extract the executables' paths
 # Note that this command will not actually run any tests
 $json = cargo test --tests --no-run --message-format=json | ConvertFrom-Json
@@ -17,21 +15,21 @@ $executables = $test_json | ForEach-Object { Write-Output $_.executable }
 
 # Format the executable paths in a way that cooperates with llvm-cov
 $arguments = $executables | ForEach-Object { "`"$_`"" }
-$command_fragment = $arguments -Join " -object "
+$command_fragment = $arguments -Join " --object "
 
 # Finally, run the command to create the coverage reports (calls llvm-cov's "report" command
 # internally: llvm-cov report)
 cargo cov -- report `
   "$command_fragment" `
-  -instr-profile "$profile_data_file" `
-  -ignore-filename-regex "$coverage_ignore_regex"
+  --instr-profile "$profile_data_file" `
+  --ignore-filename-regex "$coverage_ignore_regex"
 
 # Additionally, we will create the HTML-based mini-site with in-depth coverage information
 cargo cov -- show `
-  -Xdemangler=rustfilt `
+  --Xdemangler=rustfilt `
   "$command_fragment" `
-  -instr-profile "$profile_data_file" `
-  -ignore-filename-regex "$coverage_ignore_regex" `
-  -show-line-counts-or-regions `
-  -format html `
-  -output-dir "$html_output_directory"
+  --instr-profile "$profile_data_file" `
+  --ignore-filename-regex "$coverage_ignore_regex" `
+  --show-line-counts-or-regions `
+  --format html `
+  --output-dir "$html_output_directory"
